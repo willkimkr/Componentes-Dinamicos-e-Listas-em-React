8c7e86a5c6a253b5a7cfe19c778b6a197c0c5da5
willkimkr <98241898+willkimkr@users.noreply.github.com>
2024-11-16 20:29:28 +0000

    elemento de estilizacao adicionado

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import Card from './components/Card/Card'
 function App() {
   return (
     <>
-      <Card nome="Java"  imagem="https://salvatore.academy/devmon/1_java.png" />
-      <Card  nome="Kotlin" imagem="https://salvatore.academy/devmon/2_kotlin.png" />
-      <Card nome="Android" imagem="https://salvatore.academy/devmon/3_android.png" />
+      <div className="cards">
+        <Card nome="Java" imagem="https://salvatore.academy/devmon/1_java.png" />
+        <Card nome="Kotlin" imagem="https://salvatore.academy/devmon/2_kotlin.png" />
+        <Card nome="Android" imagem="https://salvatore.academy/devmon/3_android.png" />
+      </div >
     </>
   )
 }
